26b2d848ecb2d8520f2f1464455cbd03c18beb0f
junho3168 <junho3168@naver.com>
2025-03-06 12:27:40 +0900

    refactor - 카테고리 dto들 코틀린으로 변경

--- backend/src/main/java/com/programmers/pcquotation/domain/category/dto/CategoryCreateResponse.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/category/dto/CategoryCreateResponse.kt
@@ -1,13 +1,9 @@
 package com.programmers.pcquotation.domain.category.dto;
 
-import org.springframework.lang.NonNull;
 
-public record CategoryCreateResponse(
-	@NonNull
-	Long id,
 
-	@NonNull
-	String message // 예: "부품이 성공적으로 생성되었습니다."
-) {
-}
+data class CategoryCreateResponse(
+	val id: Long,
+	val message: String
+)
 
