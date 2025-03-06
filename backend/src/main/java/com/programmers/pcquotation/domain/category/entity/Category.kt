c3b80a6401dab8d2297a3781d6943635e4e783e3
junho3168 <junho3168@naver.com>
2025-03-06 14:15:00 +0900

    fix - CreateTestCategory 접근제어자 변경

--- backend/src/main/java/com/programmers/pcquotation/domain/category/entity/Category.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/category/entity/Category.kt
@@ -29,6 +29,7 @@ class Category(
 	}
 
 	companion object {
+		@JvmStatic
 		fun createTestCategory(id: Long, categoryName: String): Category {
 			return Category(id = id, category = categoryName)
 		}
