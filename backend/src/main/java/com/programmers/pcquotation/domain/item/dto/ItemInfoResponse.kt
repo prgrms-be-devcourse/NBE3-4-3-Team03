878b9c8956c58fbbc67ccf8d1be8f7845f85987e
junho3168 <junho3168@naver.com>
2025-03-07 11:33:26 +0900

    refactor - 들여쓰기 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/item/dto/ItemInfoResponse.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/item/dto/ItemInfoResponse.kt
@@ -3,18 +3,18 @@ package com.programmers.pcquotation.domain.item.dto;
 import jakarta.validation.constraints.NotBlank;
 
 data class ItemInfoResponse(
-	@field:NotBlank
-	val id: Long,
+    @field:NotBlank
+    val id: Long,
 
-	@field:NotBlank
-		val name: String,
+    @field:NotBlank
+    val name: String,
 
-	@field:NotBlank
-		val categoryId: Long,
+    @field:NotBlank
+    val categoryId: Long,
 
-	@field:NotBlank
-		val categoryName: String,
+    @field:NotBlank
+    val categoryName: String,
 
-	@field:NotBlank
-		val filename: String
+    @field:NotBlank
+    val filename: String
 )
