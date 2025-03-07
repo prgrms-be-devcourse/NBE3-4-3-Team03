217eb0b8430f3822c7e4107429e480ed39df6864
류대현 <eogus1027@naver.com>
2025-03-07 19:14:17 +0900

    fix - merge 하면서 누락된 코드 추가 및 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/estimate/controller/EstimateController.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/estimate/controller/EstimateController.kt
@@ -1,6 +1,10 @@
 package com.programmers.pcquotation.domain.estimate.controller
 
-import com.programmers.pcquotation.domain.estimate.dto.*
+import com.programmers.pcquotation.domain.chat.service.ChatRoomService
+import com.programmers.pcquotation.domain.chat.service.ChatService
+import com.programmers.pcquotation.domain.estimate.dto.EstimateCreateRequest
+import com.programmers.pcquotation.domain.estimate.dto.EstimateResponse
+import com.programmers.pcquotation.domain.estimate.dto.EstimateUpdateReqDto
 import com.programmers.pcquotation.domain.estimate.service.EstimateService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,14 +15,17 @@ import java.security.Principal
 @RestController
 @RequestMapping("/api/estimate")
 class EstimateController(
-    private val estimateService: EstimateService
+    private val estimateService: EstimateService,
+    private val chatService: ChatService,
+    private val chatRoomService: ChatRoomService
 ) {
     @PostMapping
     fun createEstimate(
         @RequestBody request: EstimateCreateRequest,
         principal: Principal
     ): ResponseEntity<String> {
-        estimateService.createEstimate(request, principal.name)
+        val estimate = estimateService.createEstimate(request, principal.name)
+        chatRoomService.createChatRoom(estimate)
         return ResponseEntity.ok().body("")
     }
 
@@ -30,6 +37,8 @@ class EstimateController(
 
     @DeleteMapping("/{id}")
     fun deleteEstimate(@PathVariable("id") id: Int): ResponseEntity<String> {
+        chatService.deleteChat(id)
+        chatRoomService.deleteChatRoom(id)
         estimateService.deleteEstimate(id)
         return ResponseEntity.ok().body("");
     }
--- backend/src/main/java/com/programmers/pcquotation/domain/estimate/service/EstimateService.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/estimate/service/EstimateService.kt
@@ -1,14 +1,12 @@
 package com.programmers.pcquotation.domain.estimate.service
 
-import com.programmers.pcquotation.domain.chat.repository.ChatRepository
-import com.programmers.pcquotation.domain.chat.repository.ChatRoomRepository
-import com.programmers.pcquotation.domain.chat.service.ChatService
 import com.programmers.pcquotation.domain.estimate.dto.*
 import com.programmers.pcquotation.domain.estimate.entity.Estimate
 import com.programmers.pcquotation.domain.estimate.entity.EstimateComponent
 import com.programmers.pcquotation.domain.estimate.repository.EstimateRepository
 import com.programmers.pcquotation.domain.estimaterequest.service.EstimateRequestService
 import com.programmers.pcquotation.domain.item.repository.ItemRepository
+import com.programmers.pcquotation.domain.seller.entitiy.Seller
 import com.programmers.pcquotation.domain.seller.service.SellerService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -61,7 +59,7 @@ class EstimateService(
 
     fun getEstimatesBySeller(id: Int): List<EstimateResponse> {
         val seller = sellerService.findById(id.toLong())
-            .orElseThrow { NoSuchElementException("존재하지 않는 판매자입니다.") }
+            .orElseThrow { NoSuchElementException("존재하지 않는 판매자입니다.") } as Seller
 
         val estimates = estimateRepository.getAllBySeller(seller)
 
