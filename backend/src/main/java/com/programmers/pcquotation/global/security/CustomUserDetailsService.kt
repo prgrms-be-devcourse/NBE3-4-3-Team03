a2368190270e5157d1c32a58f36d24e43198f56d
junho3168 <junho3168@naver.com>
2025-03-08 15:51:43 +0900

    refactor - CustomUserDetailsService 코틀린 변환, 기존 예외 던지던 부분 함수로 추출

--- backend/src/main/java/com/programmers/pcquotation/global/security/CustomUserDetailsService.kt
+++ backend/src/main/java/com/programmers/pcquotation/global/security/CustomUserDetailsService.kt
@@ -8,35 +8,34 @@ import org.springframework.stereotype.Service;
 import com.programmers.pcquotation.domain.admin.service.AdminService;
 import com.programmers.pcquotation.domain.customer.service.CustomerService;
 import com.programmers.pcquotation.domain.member.entitiy.Member;
-import com.programmers.pcquotation.domain.member.service.AuthService;
 import com.programmers.pcquotation.domain.seller.service.SellerService;
 import com.programmers.pcquotation.global.enums.UserType;
 
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-	private final CustomerService customerService;
-	private final SellerService sellerService;
-	private  final AdminService adminService;
+class CustomUserDetailsService(
+	private val customerService: CustomerService,
+	private val sellerService: SellerService,
+	private val adminService: AdminService
+) : UserDetailsService {
 
-	public Member loadUserByUsername(String username, UserType userType) throws UsernameNotFoundException {
-		Member member = switch (userType) {
-			case CUSTOMER -> customerService.findCustomerByUsername(username)
-				.orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다. username = " + username));
-			case SELLER -> sellerService.findByUserName(username)
-				.orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다. username = " + username));
-			case ADMIN -> adminService.findAdminByUsername(username)
-				.orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다. username = " + username));
-			default -> throw new UsernameNotFoundException("잘못된 UserType입니다.");
-		};
-
-		return member;
+	fun loadUserByUsername(username: String, userType: UserType): Member<*> {
+		return when (userType) {
+			UserType.CUSTOMER -> customerService.findCustomerByUsername(username)
+				.orElseThrow { throwUserNotFoundException(username) }
+			UserType.SELLER -> sellerService.findByUserName(username)
+				.orElseThrow { throwUserNotFoundException(username) }
+			UserType.ADMIN -> adminService.findAdminByUsername(username)
+				.orElseThrow { throwUserNotFoundException(username) }
+			else -> throw IllegalArgumentException("잘못된 UserType입니다.")
+		}
 	}
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		throw new UnsupportedOperationException("UserType이 필요합니다.");
+	private fun throwUserNotFoundException(username: String): UsernameNotFoundException {
+		return UsernameNotFoundException("해당 유저가 존재하지 않습니다. username = $username")
+	}
+
+	override fun loadUserByUsername(username: String): UserDetails {
+		throw UnsupportedOperationException("UserType이 필요합니다.")
 	}
 }
