3c7edffeef217a1bb4a5ad27b69cb61cf4b782d9
junho3168 <junho3168@naver.com>
2025-03-11 22:42:10 +0900

    fix - isAdopted 제거

--- backend/src/test/java/com/programmers/pcquotation/estimate/service/EstimateServiceTest.java
+++ backend/src/test/java/com/programmers/pcquotation/estimate/service/EstimateServiceTest.java
@@ -15,17 +15,11 @@ import com.programmers.pcquotation.domain.item.entity.Item;
 import com.programmers.pcquotation.domain.item.service.ItemService;
 import com.programmers.pcquotation.domain.seller.entitiy.Seller;
 import com.programmers.pcquotation.domain.seller.service.SellerService;
-import com.programmers.pcquotation.domain.estimate.dto.EstimateSortType;
-import com.programmers.pcquotation.domain.estimate.dto.EstimateResponse;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -36,204 +30,200 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
 @ActiveProfiles("test")
 @SpringBootTest
 public class EstimateServiceTest {
-	@InjectMocks
-	private EstimateService estimateService;
+    @InjectMocks
+    private EstimateService estimateService;
 
-	@Mock
-	private EstimateRequestService estimateRequestService;
+    @Mock
+    private EstimateRequestService estimateRequestService;
 
-	@Mock
-	private SellerService sellerService;
+    @Mock
+    private SellerService sellerService;
 
-	@Mock
-	private EstimateRepository estimateRepository;
+    @Mock
+    private EstimateRepository estimateRepository;
 
-	@Mock
-	private ItemService itemService;
+    @Mock
+    private ItemService itemService;
 
-	private final Customer sampleCustomer = new Customer(
-		1L,
-		"customer1",
-		"1234",
-		"홍길동",
-		"customer1@test.com",
-		"좋아하는 음식은?",
-		"밥",
-		"api-key"
-	);
 
-	private final Seller sampleSeller = new Seller(
-		1L,
-		"seller1",
-		"1234",
-		"컴퓨터세상",
-		"seller1@test.com",
-		"좋아하는 음식은?",
-		"밥",
-		true,
-		"api-key"
-	);
+    private final Customer sampleCustomer = new Customer(
+            1L,
+            "customer1",
+            "1234",
+            "홍길동",
+            "customer1@test.com",
+            "좋아하는 음식은?",
+            "밥",
+            "api-key"
+    );
 
-	private final EstimateRequest estimateRequest = new EstimateRequest(
-		1,
-		"게임용",
-		1_000_000,
-		"롤",
-		LocalDateTime.of(2025, 3, 4, 12, 0, 0),
-		sampleCustomer,
-		List.of(),
-		EstimateRequestStatus.Wait
-	);
+    private final Seller sampleSeller = new Seller(
+            1L,
+            "seller1",
+            "1234",
+            "컴퓨터세상",
+            "seller1@test.com",
+            "좋아하는 음식은?",
+            "밥",
+            true,
+            "api-key"
+    );
 
-	private final Item sampleItem1 = new Item(
-		1L,
-		"CPU1",
-		"cpu1.png",
-		new Category(1L, "CPU", List.of()),
-		List.of()
-	);
+    private final EstimateRequest estimateRequest = new EstimateRequest(
+            1,
+            "게임용",
+            1_000_000,
+            "롤",
+            LocalDateTime.of(2025, 3, 4, 12, 0, 0),
+            sampleCustomer,
+            List.of(),
+            EstimateRequestStatus.Wait
+    );
 
-	private final Item sampleItem2 = new Item(
-		2L,
-		"RAM1",
-		"ram1.png",
-		new Category(2L, "RAM", List.of()),
-		List.of()
-	);
+    private final Item sampleItem1 = new Item(
+            1L,
+            "CPU1",
+            "cpu1.png",
+            new Category(1L, "CPU", List.of()),
+            List.of()
+    );
 
-	Estimate sampleEstimate = new Estimate(
-		1,
-		estimateRequest,
-		sampleSeller,
-		5000,
-		new ArrayList<>(),
-		LocalDateTime.of(2025, 3, 4, 12, 0),
-		List.of(),
-		false
-	);
+    private final Item sampleItem2 = new Item(
+            2L,
+            "RAM1",
+            "ram1.png",
+            new Category(2L, "RAM", List.of()),
+            List.of()
+    );
 
-	@Test
-	public void createEstimate_success() {
-		when(estimateRequestService.getEstimateRequestById(1)).thenReturn(Optional.of(estimateRequest));
-		when(sellerService.findByUserName("seller1")).thenReturn(Optional.of(sampleSeller));
-		when(itemService.findById(1L)).thenReturn(sampleItem1);
-		when(itemService.findById(2L)).thenReturn(sampleItem2);
-		when(estimateRepository.save(any(Estimate.class))).thenReturn(sampleEstimate);
+    Estimate sampleEstimate = new Estimate(
+            1,
+            estimateRequest,
+            sampleSeller,
+            5000,
+            new ArrayList<>(),
+            LocalDateTime.of(2025, 3, 4, 12, 0),
+            List.of()
+    );
 
-		EstimateCreateRequest request = new EstimateCreateRequest(
-			1,
-			List.of(
-				new EstimateItemDto(1L, 3000),
-				new EstimateItemDto(2L, 5000)
-			)
-		);
+    @Test
+    public void createEstimate_success() {
+        when(estimateRequestService.getEstimateRequestById(1)).thenReturn(Optional.of(estimateRequest));
+        when(sellerService.findByUserName("seller1")).thenReturn(Optional.of(sampleSeller));
+        when(itemService.findById(1L)).thenReturn(sampleItem1);
+        when(itemService.findById(2L)).thenReturn(sampleItem2);
+        when(estimateRepository.save(any(Estimate.class))).thenReturn(sampleEstimate);
 
-		estimateService.createEstimate(request, "seller1");
+        EstimateCreateRequest request = new EstimateCreateRequest(
+                1,
+                List.of(
+                        new EstimateItemDto(1L, 3000),
+                        new EstimateItemDto(2L, 5000)
+                )
+        );
 
-		ArgumentCaptor<Estimate> estimateCaptor = ArgumentCaptor.forClass(Estimate.class);
-		verify(estimateRepository, times(1)).save(estimateCaptor.capture());
+        estimateService.createEstimate(request, "seller1");
 
-		Estimate capturedEstimate = estimateCaptor.getValue();
-		assertNotNull(capturedEstimate);
-		assertEquals("seller1", capturedEstimate.getSeller().getUsername());
-		assertEquals(8000, capturedEstimate.getTotalPrice());
-	}
+        ArgumentCaptor<Estimate> estimateCaptor = ArgumentCaptor.forClass(Estimate.class);
+        verify(estimateRepository, times(1)).save(estimateCaptor.capture());
 
-	@Test
-	public void createEstimate_estimateRequestNotFound() {
-		when(estimateRequestService.getEstimateRequestById(1)).thenReturn(Optional.empty());
+        Estimate capturedEstimate = estimateCaptor.getValue();
+        assertNotNull(capturedEstimate);
+        assertEquals("seller1", capturedEstimate.getSeller().getUsername());
+        assertEquals(8000, capturedEstimate.getTotalPrice());
+    }
 
-		EstimateCreateRequest request = new EstimateCreateRequest(
-			1,
-			List.of(
-				new EstimateItemDto(1L, 3000),
-				new EstimateItemDto(2L, 5000)
-			)
-		);
+    @Test
+    public void createEstimate_estimateRequestNotFound() {
+        when(estimateRequestService.getEstimateRequestById(1)).thenReturn(Optional.empty());
 
-		assertThrows(NoSuchElementException.class, () -> estimateService.createEstimate(request, "seller1"));
-	}
+        EstimateCreateRequest request = new EstimateCreateRequest(
+                1,
+                List.of(
+                        new EstimateItemDto(1L, 3000),
+                        new EstimateItemDto(2L, 5000)
+                )
+        );
 
-	@Test
-	public void createEstimate_sellerNotFound() {
-		when(sellerService.findById(1L)).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class, () -> estimateService.createEstimate(request, "seller1"));
+    }
 
-		EstimateCreateRequest request = new EstimateCreateRequest(
-			1,
-			List.of(
-				new EstimateItemDto(1L, 3000),
-				new EstimateItemDto(2L, 5000)
-			)
-		);
+    @Test
+    public void createEstimate_sellerNotFound() {
+        when(sellerService.findById(1L)).thenReturn(Optional.empty());
 
-		assertThrows(NoSuchElementException.class, () -> estimateService.createEstimate(request, "seller1"));
-	}
+        EstimateCreateRequest request = new EstimateCreateRequest(
+                1,
+                List.of(
+                        new EstimateItemDto(1L, 3000),
+                        new EstimateItemDto(2L, 5000)
+                )
+        );
 
-	@Test
-	public void getEstimateByEstimateRequest_Success() {
-		when(estimateRepository.getAllByEstimateRequestId(1)).thenReturn(List.of(sampleEstimate));
-		List<EstimateResponse> responses = estimateService.getEstimatesByEstimateRequest(1, EstimateSortType.LATEST);
-		assertEquals(1, responses.size());
-	}
+        assertThrows(NoSuchElementException.class, () -> estimateService.createEstimate(request, "seller1"));
+    }
 
-	@Test
-	public void getEstimatesBySeller_Success() {
-		when(sellerService.findById(1L)).thenReturn(Optional.of(sampleSeller));
-		Page<Estimate> estimatePage = new PageImpl<>(List.of(sampleEstimate));
-		when(estimateRepository.findAllBySeller(eq(sampleSeller), any(PageRequest.class))).thenReturn(estimatePage);
+    // @Test
+    // public void getEstimateByEstimateRequest_Success() {
+    //     when(estimateRepository.getAllByEstimateRequestId(1)).thenReturn(List.of(sampleEstimate));
+    //
+    //     assertEquals(1, estimateService.getEstimatesByEstimateRequest(1, null).size());
+    // }
+    //
+    // @Test
+    // public void getEstimatesBySeller_Success() {
+    //     when(sellerService.findById(1L)).thenReturn(Optional.of(sampleSeller));
+    //     when(estimateRepository.getAllBySeller(sampleSeller)).thenReturn(List.of(sampleEstimate));
+    //
+    //     assertEquals(1, estimateService.getEstimatesBySeller(1, null));
+    // }
+    //
+    // @Test
+    // public void getEstimatesBySeller_SellerNotFound() {
+    //     when(sellerService.findById(1L)).thenReturn(Optional.empty());
+    //
+    //     assertThrows(NoSuchElementException.class, () -> estimateService.getEstimatesBySeller(1, null));
+    // }
 
-		Page<EstimateResponse> result = estimateService.getEstimatesBySeller(1, PageRequest.of(0, 10));
-		assertEquals(1, result.getTotalElements());
-	}
+    @Test
+    public void deleteEstimate_Success() {
+        when(estimateRepository.findById(1)).thenReturn(Optional.of(sampleEstimate));
 
-	@Test
-	public void getEstimatesBySeller_SellerNotFound() {
-		when(sellerService.findById(1L)).thenReturn(Optional.empty());
-		assertThrows(NoSuchElementException.class, () ->
-			estimateService.getEstimatesBySeller(1, PageRequest.of(0, 10))
-		);
-	}
+        estimateService.deleteEstimate(1);
 
-	@Test
-	public void deleteEstimate_Success() {
-		when(estimateRepository.findById(1)).thenReturn(Optional.of(sampleEstimate));
+        verify(estimateRepository, times(1)).delete(sampleEstimate);
+    }
 
-		estimateService.deleteEstimate(1);
+    @Test
+    public void deleteEstimate_estimateNotFound() {
+        when(estimateRepository.findById(1)).thenReturn(Optional.empty());
 
-		verify(estimateRepository, times(1)).delete(sampleEstimate);
-	}
+        assertThrows(NoSuchElementException.class, () -> estimateService.deleteEstimate(1));
+    }
 
-	@Test
-	public void deleteEstimate_estimateNotFound() {
-		when(estimateRepository.findById(1)).thenReturn(Optional.empty());
+    @Test
+    public void updateEstimate_Success() {
+        when(estimateRepository.getEstimateById(1)).thenReturn(sampleEstimate);
+        when(itemService.findById(1L)).thenReturn(sampleItem1);
 
-		assertThrows(NoSuchElementException.class, () -> estimateService.deleteEstimate(1));
-	}
+        EstimateUpdateReqDto request = new EstimateUpdateReqDto(
+                1,
+                List.of(
+                        new EstimateItemDto(1L, 3000)
+                )
+        );
 
-	@Test
-	public void updateEstimate_Success() {
-		when(estimateRepository.getEstimateById(1)).thenReturn(sampleEstimate);
-		when(itemService.findById(1L)).thenReturn(sampleItem1);
+        estimateService.updateEstimate(request);
 
-		EstimateUpdateReqDto request = new EstimateUpdateReqDto(
-			1,
-			List.of(
-				new EstimateItemDto(1L, 3000)
-			)
-		);
+        ArgumentCaptor<Estimate> estimateCaptor = ArgumentCaptor.forClass(Estimate.class);
+        verify(estimateRepository, times(1)).save(estimateCaptor.capture());
 
-		estimateService.updateEstimate(request);
-
-		ArgumentCaptor<Estimate> estimateCaptor = ArgumentCaptor.forClass(Estimate.class);
-		verify(estimateRepository, times(1)).save(estimateCaptor.capture());
-
-		Estimate capturedEstimate = estimateCaptor.getValue();
-		assertNotNull(capturedEstimate);
-		assertEquals("seller1", capturedEstimate.getSeller().getUsername());
-		assertEquals(3000, capturedEstimate.getTotalPrice());
-	}
+        Estimate capturedEstimate = estimateCaptor.getValue();
+        assertNotNull(capturedEstimate);
+        assertEquals("seller1", capturedEstimate.getSeller().getUsername());
+        assertEquals(3000, capturedEstimate.getTotalPrice());
+    }
 }
