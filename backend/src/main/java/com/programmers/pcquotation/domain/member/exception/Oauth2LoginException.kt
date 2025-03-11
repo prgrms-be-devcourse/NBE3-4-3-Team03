5d045732d9ff3f9c564b37d595e0490e2458c18a
UCLABgown <lovecein4858@gmail.com>
2025-03-11 23:49:14 +0900

    #22 refactor - oauth2 exception 메시지null 안들어오게 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/member/exception/Oauth2LoginException.kt
+++ backend/src/main/java/com/programmers/pcquotation/domain/member/exception/Oauth2LoginException.kt
@@ -1,3 +1,3 @@
 package com.programmers.pcquotation.domain.member.exception
 
-class Oauth2LoginException(message: String?) : RuntimeException(message)
+class Oauth2LoginException(message: String) : RuntimeException(message)
